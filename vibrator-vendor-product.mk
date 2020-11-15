8fe1d81984cb84a78ba0b53e3a6c76b2a9802bd8
Michael Bestas <mkbestas@lineageos.org>
2020-11-15 02:47:17 +0200

    Merge tag 'LA.UM.9.12.r1-09000-SMxx50.0' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/vibrator into lineage-18.0
    "LA.UM.9.12.r1-09000-SMxx50.0"
    
    Change-Id: I1e3ba1db07838c73a68d6790cb6b7df47a0f40ec

--- vibrator-vendor-product.mk
+++ vibrator-vendor-product.mk
@@ -1,4 +1,4 @@
-ifeq ($(call is-board-platform-in-list, msmnile kona lahaina shima lito atoll sm6150 msm8953 msm8937 trinket bengal bengal_32 bengal_32go), true)
+ifeq ($(call is-board-platform-in-list, msmnile kona lahaina shima lito atoll sm6150 msm8953 msm8937 trinket bengal bengal_32 bengal_32go holi sdm660), true)
 
 QTI_VIBRATOR_HAL_SERVICE := \
       vendor.qti.hardware.vibrator.service
