fc2ed8460e436dcc82b64edbc5f701a8a7433353
Jishnu Prakash <jprakash@codeaurora.org>
2021-01-05 13:41:04 +0530

    Vibrator: add support for yupik
    Yupik QRD uses PM8350B which supports external control via
    sound-wire interface, hence enable the same.
    
    Change-Id: I2d7fa0424aaaa151ac0e6db3d9c7e1d4ddc61824
    Signed-off-by: Jishnu Prakash <jprakash@codeaurora.org>

--- aidl/Vibrator.cpp
+++ aidl/Vibrator.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -121,6 +121,7 @@ InputFFDevice::InputFFDevice()
             case MSM_CPU_SHIMA:
             case MSM_CPU_SM8325:
             case APQ_CPU_SM8325P:
+            case MSM_CPU_YUPIK:
                 mSupportExternalControl = true;
                 break;
             default:
