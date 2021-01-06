6a061cf7f43dbd8c8b13da5c89ef36f184da7509
Kavya Nunna <knunna@codeaurora.org>
2021-01-06 18:49:29 +0530

    vibrator: Remove target check in makefile
    Remove target check for vibrator in makefile as it
    is not required for any target.
    
    Change-Id: I9448b06cc6a4b584e03dba2d02156ed5d685c039

--- vibrator-vendor-product.mk
+++ vibrator-vendor-product.mk
@@ -1,5 +1,3 @@
-ifeq ($(call is-board-platform-in-list, msmnile kona lahaina shima lito atoll sm6150 msm8953 msm8937 trinket bengal bengal_32 bengal_32go holi sdm660), true)
-
 QTI_VIBRATOR_HAL_SERVICE := \
       vendor.qti.hardware.vibrator.service
 
@@ -7,4 +5,3 @@ PRODUCT_PACKAGES += $(QTI_VIBRATOR_HAL_SERVICE)
 
 PRODUCT_COPY_FILES += \
       vendor/qcom/opensource/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
-endif
