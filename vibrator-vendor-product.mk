e5282a48505ee455cc05e4eea0e8258b28b2100e
Kiran Gunda <kgunda@codeaurora.org>
2021-03-24 15:50:49 +0530

    Vibrator: Add support to offload the patterns
    Extend the Vibrator HAL to support offloading the predefined
    patterns to remote processor in a multi-proc system.
    The applications which uses the vibrator can map their events
    to the predefined patterns to maintain the same vibration effect
    for an event on the remote proc after offloading.
    
    Change-Id: I049ad620a2e4aba461d3dd0194329e19b5ffed18

--- vibrator-vendor-product.mk
+++ vibrator-vendor-product.mk
@@ -1,6 +1,10 @@
+ifeq ($(HAPTICS_OFFLOAD_SUPPORT),true)
+QTI_VIBRATOR_HAL_SERVICE := \
+      vendor.qti.hardware.vibrator.offload.service
+else
 QTI_VIBRATOR_HAL_SERVICE := \
       vendor.qti.hardware.vibrator.service
-
+endif
 PRODUCT_PACKAGES += $(QTI_VIBRATOR_HAL_SERVICE)
 
 PRODUCT_COPY_FILES += \
