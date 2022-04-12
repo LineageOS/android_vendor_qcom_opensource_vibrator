a261aa36998b550e2041f356a4712b8a3a3ec3f6
Hui Liu <quic_huliu@quicinc.com>
2022-04-12 17:42:23 +0800

    Vibrator: add compose effect support
    Add compose effect support and implement all related APIs.
    
    Change-Id: I88ddb8340cf4e84aac9e177ea48b4fddca5fad05

--- aidl/include/Vibrator.h
+++ aidl/include/Vibrator.h
@@ -25,11 +25,16 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #pragma once
 
 #include <aidl/android/hardware/vibrator/BnVibrator.h>
+#include <thread>
 
 namespace aidl {
 namespace android {
@@ -40,6 +45,7 @@ class InputFFDevice {
 public:
     InputFFDevice();
     int playEffect(int effectId, EffectStrength es, long *playLengthMs);
+    int playPrimitive(int primitiveId, float amplitude, long *playLengthMs);
     int on(int32_t timeoutMs);
     int off();
     int setAmplitude(uint8_t amplitude);
@@ -47,6 +53,7 @@ public:
     bool mSupportEffects;
     bool mSupportExternalControl;
     bool mInExternalControl;
+
 private:
     int play(int effectId, uint32_t timeoutMs, long *playLengthMs);
     int mVibraFd;
@@ -68,6 +75,9 @@ class Vibrator : public BnVibrator {
 public:
     class InputFFDevice ff;
     class LedVibratorDevice ledVib;
+    Vibrator();
+    ~Vibrator();
+
     ndk::ScopedAStatus getCapabilities(int32_t* _aidl_return) override;
     ndk::ScopedAStatus off() override;
     ndk::ScopedAStatus on(int32_t timeoutMs,
@@ -98,6 +108,14 @@ public:
     ndk::ScopedAStatus getSupportedBraking(std::vector<Braking>* supported) override;
     ndk::ScopedAStatus composePwle(const std::vector<PrimitivePwle> &composite,
                                const std::shared_ptr<IVibratorCallback> &callback) override;
+private:
+    static void composePlayThread(Vibrator *vibrator,
+                        const std::vector<CompositeEffect>& composite,
+                        const std::shared_ptr<IVibratorCallback>& callback);
+    std::thread composeThread;
+    int epollfd;
+    int pipefd[2];
+    std::atomic<bool> inComposition;
 };
 
 }  // namespace vibrator
