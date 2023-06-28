9d3c42eb02d777f63d3d00b3d177da87c4b5feb6
Richard Raya <rdxzv.dev@gmail.com>
2023-06-27 23:58:26 -0300

    vibrator: Add support for drv2624:haptics
    Change-Id: Ic5ac8c4133db3f9e808cb3948ee8b7d0e8a97aea

--- aidl/VibratorOL/Vibrator.cpp
+++ aidl/VibratorOL/Vibrator.cpp
@@ -137,7 +137,8 @@ InputFFDevice::InputFFDevice()
                 && strcmp(name, "aw8624_haptic")
                 && strcmp(name, "aw8695_haptic")
                 && strcmp(name, "aw8697_haptic")
-                && strcmp(name, "awinic_haptic")) {
+                && strcmp(name, "awinic_haptic")
+                && strcmp(name, "drv2624:haptics")) {
             ALOGD("not a supported haptics device\n");
             close(fd);
             continue;
