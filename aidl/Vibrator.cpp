6c21db37fdcb91bf044da8807ed233f2ccf6ca87
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-02 22:59:16 +0200

    vibrator: automatically detect supported effects
    Change-Id: I879f229ce04db390dfcf3ac9bc7ab937cc82b66d

--- aidl/Vibrator.cpp
+++ aidl/Vibrator.cpp
@@ -612,10 +612,12 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es, const std
             (effect > Effect::RINGTONE_15))
             return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
     }
+#ifndef USE_EFFECT_STREAM
     else {
         if (effect < Effect::CLICK ||  effect > Effect::HEAVY_CLICK)
             return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
     }
+#endif
 
     if (es != EffectStrength::LIGHT && es != EffectStrength::MEDIUM && es != EffectStrength::STRONG)
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
@@ -641,13 +643,27 @@ ndk::ScopedAStatus Vibrator::getSupportedEffects(std::vector<Effect>* _aidl_retu
     if (ledVib.mDetected)
         return ndk::ScopedAStatus::ok();
 
-    if (Offload.mEnabled == 1)
+    if (Offload.mEnabled == 1) {
         *_aidl_return = {Effect::CLICK, Effect::DOUBLE_CLICK, Effect::TICK, Effect::THUD,
                          Effect::POP, Effect::HEAVY_CLICK, Effect::RINGTONE_12,
                          Effect::RINGTONE_13, Effect::RINGTONE_14, Effect::RINGTONE_15};
-    else
-        *_aidl_return = {Effect::CLICK, Effect::DOUBLE_CLICK, Effect::TICK, Effect::THUD,
-                         Effect::POP, Effect::HEAVY_CLICK};
+        return ndk::ScopedAStatus::ok();
+    }
+
+#ifndef USE_EFFECT_STREAM
+    *_aidl_return = {Effect::CLICK, Effect::DOUBLE_CLICK, Effect::TICK, Effect::THUD,
+                     Effect::POP, Effect::HEAVY_CLICK};
+#else
+    for (int32_t effectId = static_cast<int32_t>(Effect::CLICK);
+         effectId <= static_cast<int32_t>(Effect::TEXTURE_TICK);
+         effectId++) {
+        const struct effect_stream *stream;
+
+        stream = get_effect_stream(effectId);
+        if (stream)
+            _aidl_return->push_back(static_cast<Effect>(effectId));
+    }
+#endif
 
     return ndk::ScopedAStatus::ok();
 }
