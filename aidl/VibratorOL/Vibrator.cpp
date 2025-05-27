923fdc2b47c85dba092dda43ed929a89bb58af98
Alexander Koskovich <akoskovich@pm.me>
2025-05-27 15:53:25 -0400

    vibrator: Add support for haptic_rt input device
    Change-Id: I850cdf6e6770e2fdfbd7654b46f3409cc8627f5d

--- aidl/VibratorOL/Vibrator.cpp
+++ aidl/VibratorOL/Vibrator.cpp
@@ -140,7 +140,8 @@ InputFFDevice::InputFFDevice()
                 && strcmp(name, "aw8697_haptic")
                 && strcmp(name, "awinic_haptic")
                 && strcmp(name, "drv2624:haptics")
-                && strcmp(name, "si_haptic")) {
+                && strcmp(name, "si_haptic")
+                && strcmp(name, "haptic_rt")) {
             ALOGD("not a supported haptics device\n");
             close(fd);
             continue;
