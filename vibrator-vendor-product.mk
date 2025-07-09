807eaca049abacf4bfe21494cda255e264361769
Onelots <onelots@onelots.fr>
2025-07-09 11:37:07 +0200

    Revert "vibrator: Remove hardcoded path"
    This reverts commit fdadae253cdf712751d1e925abd41bb6eeb18aad.
    
    *  This commit will allow us to use back the fragment, instead of
    inheriting manually what's already in the mk file.
    
    * In this commit, the flag BOARD_OPENSOURCE_DIR was set, which used
    to break audio building by setting a bad path for audio source under
    certain circumstances, as referenced here for example :
    https://github.com/LineageOS/android_hardware_qcom_audio/blob/9e24e3a3512573f21c1cfbf8d2f0c1c419526a4d/hal/audio_extn/Android.mk#L3-L10
    
    * As a matter of fact, the following error happened on an external
    device (veux, xiaomi sm6375):
    ```
    hardware/qcom-caf/sm8350/audio/hal/audio_extn/sndmonitor.c:60:10: fatal error: 'audio_hw.h' file not found
       60 | #include "audio_hw.h"
          |          ^~~~~~~~~~~~
    1 error generated.```
    
    Inheriting the vibrator without using the fragment fixed it, which means
    the issue was actually the BOARD_OPENSOURCE_DIR flag.
    
    * Moreover, BOARD_OPENSOURCE_DIR is actually set to a value in the same file.
    Reverting should not be harmful.
    
    Change-Id: I9ed945288f432c7379733734c47afe4586eeee25
    Signed-off-by: Onelots <onelots@onelots.fr>

--- vibrator-vendor-product.mk
+++ vibrator-vendor-product.mk
@@ -1,6 +1,5 @@
 QTI_VIBRATOR_HAL_SERVICE := vendor.qti.hardware.vibrator.service
 PRODUCT_PACKAGES += $(QTI_VIBRATOR_HAL_SERVICE)
 
-BOARD_OPENSOURCE_DIR ?= vendor/qcom/opensource
 PRODUCT_COPY_FILES += \
-	$(BOARD_OPENSOURCE_DIR)/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
+      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
